b38b8a72a5cf4441bfe244df5dbf682eae68d92a
i300289 <smarika.7sharma@gmail.com>
2019-01-17 13:44:41 +0530

    some sols

--- src/main/Scala99.scala
+++ src/main/Scala99.scala
@@ -3,4 +3,20 @@ package main
 object Scala99 {
 
 
+  def main (args:Array[String]) = {
+
+ //p01 last(List(1, 1, 2, 3, 5, 8))
+
+    def last[T](ls:List[T]):T = {
+      ls.last
+    }
+
+    println("the last element is:" +last[String](List("a","b")))
+
+    //p02 penultimate(List(1, 1, 2, 3, 5, 8))
+
+
+  }
+
+
 }
