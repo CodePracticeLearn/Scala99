373b861221deee2ad43e7ab3b7e82712febc7702
i300289 <smarika.7sharma@gmail.com>
2019-01-18 11:14:03 +0530

    changes

--- src/main/Scala99Problems.scala
+++ src/main/Scala99Problems.scala
@@ -17,9 +17,9 @@ object Scala99Problems {
     //p02 penultimate(List(1, 1, 2, 3, 5, 8))
 
     def penultimate[T](xs: List[T]): T = xs match {
-      case x :: y :: Nil  => x
+      case x :: y :: Nil => x
       case x :: y :: rest => penultimate(y :: rest)
-      case _              => throw new Exception("error while reading List")
+      case _ => throw new Exception("error while reading List")
     }
 
     println("last but one element" + penultimate(List("a", "b")))
@@ -29,8 +29,8 @@ object Scala99Problems {
     @tailrec
     def nth[T](n: Int, list: List[T]): Option[T] = (n, list) match {
       case (0, head :: tail) => Some(head)
-      case (_, Nil)          => None
-      case (x, _) if x < 0   => None
+      case (_, Nil) => None
+      case (x, _) if x < 0 => None
       case (x, head :: tail) => nth(x - 1, tail)
     }
 
@@ -40,7 +40,7 @@ object Scala99Problems {
 
     def length[T](ls: List[T]): Int = {
       ls match {
-        case Nil          => 0
+        case Nil => 0
         case head :: tail => 1 + length(tail)
       }
     }
@@ -51,9 +51,9 @@ object Scala99Problems {
 
     def reverse[T](ls: List[T]): List[T] = {
       ls match {
-        case Nil      => Nil
+        case Nil => Nil
         case x :: Nil => List(x)
-        case x :: y   => reverse(y) ::: List(x)
+        case x :: y => reverse(y) ::: List(x)
       }
     }
 
@@ -69,9 +69,9 @@ object Scala99Problems {
 
     def flatten(ls: Any): List[Any] = { //can also be done with ls:List[Any]
       ls match {
-        case Nil        => Nil
+        case Nil => Nil
         case head :: xs => flatten(head) ::: flatten(xs)
-        case q          => List(q)
+        case q => List(q)
       }
     }
 
@@ -81,7 +81,7 @@ object Scala99Problems {
 
     def compress[T](ls: List[T]): List[T] = {
       ls match {
-        case Nil         => Nil
+        case Nil => Nil
         case head :: Nil => ls
         case first :: second :: xs =>
           if (first == second)
@@ -101,7 +101,7 @@ object Scala99Problems {
     def pack[T](list: List[T]): List[List[T]] = {
 
       def packInner(acc: List[T], l: List[T]): List[List[T]] = l match {
-        case head :: tail if acc.isEmpty      => packInner(List(head), tail)
+        case head :: tail if acc.isEmpty => packInner(List(head), tail)
         case head :: tail if head == acc.head => packInner(head :: acc, tail)
         case head :: tail if head != acc.head =>
           acc :: packInner(List(head), tail)
@@ -139,7 +139,7 @@ object Scala99Problems {
         case x =>
           pack(x).map { elemList =>
             elemList.size match {
-              case 1  => elemList.headOption
+              case 1 => elemList.headOption
               case sz => (sz, elemList.headOption)
             } // remove headOption here and get the usual result
           }
@@ -174,7 +174,7 @@ object Scala99Problems {
   def duplicate[T](ls: List[T]) = {
     ls match {
       case Nil => List()
-      case xs  => xs.map(a => (a, a))
+      case xs => xs.map(a => (a, a))
 
     }
   }
@@ -185,7 +185,7 @@ object Scala99Problems {
   //p15  duplicateN(3, List('a, 'b, 'c, 'c, 'd))
   //res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
 
-  def duplicateN[T](n:Int,ls:List[T])={
+  def duplicateN[T](n: Int, ls: List[T]) = {
     ls.flatMap(a => (1 to n).map(_ => a))
 
   }
@@ -195,12 +195,39 @@ object Scala99Problems {
   //p16 drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
   //res0: List[Symbol] = List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
 
-  def drop[T](n:Int, ls:List[T]) ={
+  def drop[T](n: Int, ls: List[T]) = {
     //failure cases like list blank etc are not written
-    ls.zipWithIndex filter { e => (e._2 + 1) % n != 0 } map { _._1 }
+    ls.zipWithIndex filter { e => (e._2 + 1) % n != 0 } map {
+      _._1
+    }
+  }
+
+
+  println("dropped" + drop(5, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+
+  //hereafter problems are solved with existing list methods , to get to know these methods, check the
+  //source code
+
+  //p17 split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+
+  def splitN[T](n: Int, ls: List[T]) = {
+    //failure cases like list blank etc are not written
+    (ls.slice(0,n),ls.slice(n+1,ls.length))
     }
 
+  println("split of list"+splitN(4, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
 
-  println("dropped"+ drop(5, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+  //p18 scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //res0: List[Symbol] = List('d, 'e, 'f, 'g)
+
+ println("sliced"+List(1,2,3,4,5,6).slice(3,7))
+
+  //p19 rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //res1: List[Symbol] = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+
+
+
+
+
 }
-
